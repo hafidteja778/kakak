d318d6e8689195f837a7338a5314ba85fa97657e
hafidteja778 <58174605+hafidteja778@users.noreply.github.com>
2020-03-18 09:48:30 +0700

    Update ndeso.php

--- ndeso.php
+++ ndeso.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
-include "function.php";
+include "game.php";
 echo color("blue"," ===========================\n");
 echo color("green","| LOSS GAK REWEL |\n");
 echo color("green","| Auto CLAIM |\n");
